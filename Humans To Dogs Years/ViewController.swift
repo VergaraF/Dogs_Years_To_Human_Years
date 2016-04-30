accddd8087c1c95121bc34d20549e8a4a934f02f
Fabian Vergara <vergarf@gmail.com>
2016-04-29 21:33:07 -0400

    Finished app

--- Humans To Dogs Years/ViewController.swift
+++ Humans To Dogs Years/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userInput: UITextField!
+    @IBOutlet var age: UILabel!
+    @IBOutlet var humanYearsLabel: UILabel!
+    
+    
+    @IBAction func findAgeButton(sender: AnyObject) {
+        
+        var tempAge = 0
+        let ageGivenByUser = Int(userInput.text!)!
+        var dogsAgeCounter = 0
+        
+        
+        while (dogsAgeCounter < ageGivenByUser){
+            
+            if dogsAgeCounter == 0{
+                tempAge = tempAge + 15
+            }else if (dogsAgeCounter == 1){
+                tempAge = tempAge + 9
+            }else{
+                tempAge = tempAge + 4
+            }
+            dogsAgeCounter += 1
+            
+        }
+        
+        age.text = String(tempAge)
+        age.hidden = false
+        humanYearsLabel.hidden = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
